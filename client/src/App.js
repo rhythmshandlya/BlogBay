6283e8061c5436abc17bd4963769b2064ab985ce
Eeshan8503 <Eeshan8503@users.noreply.github.com>
2021-07-06 13:50:25 +0530

    starter

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
-import React from "react";
-
+import Blog from './Blog.js';
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Blog></Blog>
+  );
 }
 
 export default App;
---
+++ client/src/Blog.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './blog.css'
+function Blog(){
+    return(
+        <center>
+        <div className="blog_container">
+        <div className="blog">
+        <center>
+        <h1>TITLE</h1>
+        </center>
+        </div>
+        <div className="foot">
+        <h1>foot</h1>
+        </div>
+        </div>
+        </center>
+    );
+}
+
+export default Blog;
