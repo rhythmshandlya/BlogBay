250448287a8b13863365bd0f7a52908373b7237b
rhythmshandlya <armaanbgp@gmail.com>
2021-07-27 22:52:06 +0530

    Some minor refactor

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import Navbar from "./Components/NavbarComponents/Navbar";
-import Profile from "./Components/NavbarComponents/Profile.jsx";
+import Profile from "./Components/ProfileComponent/Profile.jsx";
 import "./app.css";
 
 function App() {
