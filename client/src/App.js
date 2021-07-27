36b850d63eead0e0aaf476b9aa40b3e908998a3e
Eeshan8503 <Eeshan8503@users.noreply.github.com>
2021-07-27 18:43:06 +0530

    profile1

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import Navbar from "./Components/NavbarComponents/Navbar";
+import Profile from "./Components/NavbarComponents/Profile.jsx";
 import "./app.css";
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
+      {/* <Profile></Profile> */}
+
     </>
   );
 }
--- client/src/Components/NavbarComponents/Profile.jsx
+++ client/src/Components/NavbarComponents/Profile.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import './Stylesheets/profile.css';
 const Profile = () => {
     return (
         <div>
-            
+            <div className="cover">
+                <div className="avatar"></div>
+            </div>
+            <div className="info">
+                <p className="info_links" >About</p>
+                <p className="info_links" style={{paddingLeft:"3em"}}>Blogs</p>
+                <p className="info_links">Personal Info</p>
+            </div>
         </div>
     )
 }
