8b72f6c14ccf408cb4207bf6c9b16233914f5bcb
rhythmshandlya <armaanbgp@gmail.com>
2021-07-28 16:01:42 +0530

    error

--- Controllers/blogController.js
+++ Controllers/blogController.js
@@ -1,5 +1,43 @@
-exports.getAllBlogs = () => {};
-exports.addBlog = () => {};
-exports.getBlog = () => {};
-exports.updateBlog = () => {};
-exports.deleteBlog = () => {};
+const Blog = './../Models/blogModel.js';
+
+exports.getAllBlogs = (req, res) => {
+  try {
+    const allBlogs = Blog.find({});
+    res.status(200).json({
+      status: true,
+      data: {
+        allBlogs
+      }
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({
+      status: false,
+      err
+    });
+  }
+};
+
+exports.addBlog = async (req, res) => {
+  try {
+    const newBlog = await Blog.create(req.body);
+    console.log(newBlog);
+    res.status(201).json({
+      status: true,
+      data: {
+        title: newBlog.title,
+        date: newBlog.date,
+        authorID: newBlog.authorID
+      }
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: false,
+      error
+    });
+  }
+};
+
+exports.getBlog = (req, res) => {};
+exports.updateBlog = (req, res) => {};
+exports.deleteBlog = (req, res) => {};
--- Models/blogModel.js
+++ Models/blogModel.js
@@ -1,1 +1,27 @@
-con
+const mongoose = require('mongoose');
+
+const BlogSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  summary: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    require: false,
+    default: Date.now()
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  authorID: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('blog', BlogSchema);
