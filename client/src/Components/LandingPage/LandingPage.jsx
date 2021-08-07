f96afb72023b7c91755acc9c11a8e3de23b27091
Eeshan8503 <Eeshan8503@users.noreply.github.com>
2021-08-07 13:20:29 +0530

    prototype 2

--- client/src/Components/LandingPage/LandingPage.jsx
+++ client/src/Components/LandingPage/LandingPage.jsx
@@ -1,24 +1,29 @@
-import React from 'react'
+import React ,{useEffect} from 'react'
 import './Stylesheets/LandingPage.css'
 import ButtonSignup from './ButtonSignup'
 import EasyCard from './../BlogCards/EasyCard.jsx'
 import Navbar from './../NavbarComponents/Navbar';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 const LandingPage = () => {
+    useEffect(() => {
+      Aos.init({duration:2000});
+    }, []);
     return (
         <>
         <Navbar />
         <div className="landing-page">
             <div className='landing'>
-                <h1 className="h1-landing">Write blogs with us</h1>
+                <h1 className="h1-landing" data-aos="fade-up">Write blogs with us</h1>
                 <ButtonSignup />
             </div>
-            <div className="land-about-us"></div>
+          
             <div className="sample-blogs">
                 <EasyCard></EasyCard>
                 <EasyCard></EasyCard>
                 <EasyCard></EasyCard>
             </div>
-            <div className="footer"><h1>footer</h1></div>
+            
         </div>
         </>
     )
