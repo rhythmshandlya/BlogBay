fd986d7bbba7ec28c64b62fd369e90dfdb8397a3
Eeshan8503 <Eeshan8503@users.noreply.github.com>
2021-08-12 13:23:05 +0530

    grid made responsive

--- client/src/Components/LandingPage/LandingPage.jsx
+++ client/src/Components/LandingPage/LandingPage.jsx
@@ -1,4 +1,4 @@
-import React ,{useEffect} from 'react'
+import React ,{useEffect, useState} from 'react'
 import './Stylesheets/LandingPage.css'
 import ButtonSignup from './ButtonSignup'
 import EasyCard from './../BlogCards/EasyCard.jsx'
@@ -7,6 +7,22 @@ import Aos from 'aos';
 import logo from './../../data/icon-st.svg';
 import 'aos/dist/aos.css';
 const LandingPage = () => {
+    const [numBlogs, numBlogsSetter]=useState(Math.floor(window.innerWidth/300)-1)
+    function check(){
+        numBlogsSetter(Math.floor(window.innerWidth/300)-1);
+        console.log(numBlogs)
+    }
+    function renderer(amount){
+        var EasyCardRenderer=[];
+        for(let i=0;i<amount;i++){
+            EasyCardRenderer.push(<EasyCard></EasyCard>);
+        }
+        return EasyCardRenderer;
+    }
+    // useEffect(()=>{
+    //     renderer(numBlogs)
+    // },[numBlogs]);
+    window.addEventListener("resize",check);
     useEffect(() => {
       Aos.init({duration:2000});
     }, []);
@@ -17,11 +33,8 @@ const LandingPage = () => {
     }
     function CatBlogs(){
         return(
-            <div className="sample-blogs">
-            <EasyCard></EasyCard>
-            <EasyCard></EasyCard>
-            <EasyCard></EasyCard>
-            <EasyCard></EasyCard>
+            <div className="sample-blogs" style={{gridTemplateColumns:"repeat("+numBlogs+",320px)"}}>
+                {renderer(numBlogs)}
             </div>
         )
     }
@@ -39,12 +52,14 @@ const LandingPage = () => {
             <CatBlogs></CatBlogs>
             <CatName name="Travel Blogs"></CatName>
             <CatBlogs></CatBlogs>
-        <div className="about-us">
             <CatName name="About Us"/>
+        <div className="about-us"> 
             <img src={logo} alt="us" height='500px' width='400px'/>
-            <pre>With it installed in the code editor yo
-            u are using, you can type
-             “lorem” and then tab and it will </pre>
+            <div><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id dictum arcu, sit amet vestibulum mi. Ut vestibulum non augue eget condimentum. Ut vel molestie erat, lobortis lobortis tellus. Phasellus non diam mattis, suscipit lacus vel, venenatis metus. Nullam sit amet placerat nunc. Duis sit amet bibendum felis, nec posuere erat. Maecenas consequat efficitur sodales. Donec rutrum pulvinar tortor in scelerisque. Donec molestie blandit massa et porta. In facilisis sodales tempor. Maecenas vel sodales sapien. Aliquam sed ex non tortor viverra dignissim. In interdum congue justo, nec iaculis tortor auctor id. Maecenas ut quam venenatis, iaculis orci eu, hendrerit turpis. Morbi eget erat libero.
+
+Etiam euismod at lacus sed aliquet. Vivamus tincidunt nulla a augue consequat, vitae eleifend ipsum euismod. Maecenas magna risus, porta vitae metus ultrices, convallis efficitur felis. Sed a vulputate diam. Morbi convallis ac est at venenatis. Sed quis eros auctor mauris pulvinar posuere eu ac ligula. Nunc vitae efficitur leo. Donec ipsum orci, feugiat ac lacus sit amet, finibus congue ex. Aliquam posuere elit ut viverra ultrices. Suspendisse felis orci, condimentum vitae elit eu, ultrices cursus orci. Nulla aliquet ligula est, ut tempus sem dictum quis. Nam vel tincidunt ex. Mauris ut ante dapibus, molestie elit ac, pretium tortor. Vivamus tempus imperdiet leo, at rutrum risus sollicitudin vel. Donec placerat nulla magna, sed accumsan nibh accumsan eu.
+
+Suspendisse ut posuere arcu. Maecenas pellentesque, leo id aliquet euismod, justo mi hendrerit lectus, at iaculis dolor ipsum at massa. Duis congue turpis id sapien tincidunt, non tincidunt ipsum sagittis. Donec id porttitor elit. Donec interdum risus vitae fermentum faucibus. Aliquam erat volutpat. Integer vestibulum ipsum at porttitor iaculis. Maecenas maximus feugiat turpis, aliquet vulputate quam maximus non. Phasellus laoreet erat at odio aliquam semper.</p></div>
          </div>
          <div className="footer">
              <h1>FOOTER</h1>
