726344409c52e64905f0acb95cdd2b16d83a20d5
rhythmshandlya <armaanbgp@gmail.com>
2021-08-17 17:16:32 +0530

    profile work

--- Data/dp_default.js
+++ Data/dp_default.js
@@ -5,3 +5,11 @@ exports.dps = [
   'https://i.ibb.co/r28m6vR/dp5.jpg',
   'https://i.ibb.co/Np7MBJS/dp4.jpg'
 ];
+
+exports.covers = [
+  'https://i.ibb.co/0DLTKYD/cover1.jpg',
+  'https://i.ibb.co/sR3HzhS/cover2.jpg',
+  'https://i.ibb.co/12yjPkv/cover3.jpg',
+  'https://i.ibb.co/Qjvqj8Z/cover4.jpg',
+  'https://i.ibb.co/pfWBrqK/cover3.jpg'
+];
--- client/src/Components/ProfileComponent/Profile.jsx
+++ client/src/Components/ProfileComponent/Profile.jsx
@@ -1,33 +1,68 @@
-import React from 'react'
+import api from './../../Util/api'
+import { useEffect, useState } from 'react';
 import './Stylesheets/profile.css';
 import Navbar from './../NavbarComponents/Navbar';
+import { useHistory } from 'react-router-dom';
 
-const about = (<><h1>About</h1></>);
-const blog = (<><h1>Blog</h1></>);
-const contact = (<><h1>Contact</h1></>);
-
-const backgroundImg = 'http://placeimg.com/1000/1000/nature';
-const displayPicture = 'https://picsum.photos/500/500';
+const About = ({user}) => {
+    return (
+        <>
+            <p>Name: {`${user.name}`}</p>
+            <p>Niche: {`${user.niche}`}</p>
+            <p>Job: {`${user.job}`}</p>
+            <p>Description :{`${user.description}`}</p>
+        </>
+    );
+}
+const Blogs = (props) => {
+    return (
+        <>
+        </>
+    );
+}
+const Stats = (props) => {
+    return (
+        <>
+        </>
+    );
+}
 
 const Profile = () => {
-    const [state, stateSetter] = React.useState(blog);
+    let history = useHistory();
+    const [user, setUser] = useState({});
+    const [state, stateSetter] = useState(<Blogs />);
+
+    useEffect(() => {
+        async function fetchMyAPI() {
+           try {
+               let res = await api.get('user/isLoggedIn', { withCredentials: true });
+               if (res.data.user) {
+                   setUser(res.data.user);
+               }
+           }catch (err) {
+                history.push('/login');
+           }
+       }
+       fetchMyAPI();
+    }, []);
+    
     const clickHandler = (e) => {
         const nav = document.querySelectorAll('.info_links');
         switch (e.target.textContent) {
             case 'About':
                 nav[1].classList.remove('un-p');
                 nav[2].classList.remove('un-p');
-                stateSetter(about);
+                stateSetter(<About user={user}/>);
                 break;
-            case 'Contact':
+            case 'Stats':
                 nav[0].classList.remove('un-p');
                 nav[1].classList.remove('un-p');
-                stateSetter(contact);
+                stateSetter(<Stats user={user}/>);
                 break;
             case 'Blogs':
                 nav[0].classList.remove('un-p');
                 nav[2].classList.remove('un-p');
-                stateSetter(blog);
+                stateSetter(<Blogs user={user}/>);
                 break;
             default:
              console.log('error')
@@ -37,19 +72,19 @@ const Profile = () => {
 
     return (
         <>
-        <Navbar />
-        <div>
-        <div className="CoverImage FlexEmbed FlexEmbed--2by1" style={{ backgroundImage: `url(${backgroundImg})`}}></div>
-            <div className="avatar" style={{ backgroundImage: `url(${displayPicture})` }}></div>
-            <div className="info">
-                <p className="info_links link-grow un" onClick={clickHandler}>About</p>
-                <p className="info_links un un-p" onClick={clickHandler}>Blogs</p>
-                <p className="info_links un" onClick={clickHandler}>Contact</p>
-            </div>
-            {state}
+            <Navbar />
+            <div>
+                <div className="CoverImage FlexEmbed FlexEmbed--2by1" style={{ backgroundImage: `url(${user.cover}})` }}></div>
+                <img className="avatar" src={`${user.dp}`} alt="Girl in a jacket" />
+                <div className="info">
+                    <p className="info_links link-grow un" onClick={clickHandler}>About</p>
+                    <p className="info_links un un-p" onClick={clickHandler}>Blogs</p>
+                    <p className="info_links un" onClick={clickHandler}>Stats</p>
+                </div>
+                {state}
             </div>
         </>
-    )
+    );
 }
 
 export default Profile
