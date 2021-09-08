247b736882647f4e77b3d511b8741fe19fd960b4
rhythmshandlya <armaanbgp@gmail.com>
2021-09-08 21:39:48 +0530

    new

--- client/src/Components/BlogSearch/BlogSearch.jsx
+++ client/src/Components/BlogSearch/BlogSearch.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { useState } from 'react';
 import Navbar from './../NavbarComponents/Navbar';
 import './BlogSearch.css'
 
 const BlogSearch = () => {
+    const [blogType, setBlogType] = useState(['Top Blogs']);
+
+    const handleClick = (e) => {
+        e.target.classList.toggle('crimson');
+        e.target.classList.toggle('tag-bg');
+        if (e.target.classList.contains('crimson')) {
+            setBlogType([...blogType, e.target.innerHTML]);
+        }
+        else {
+            let blogTypeTemp = [...blogType];
+            blogTypeTemp.splice(blogTypeTemp.indexOf(e.target.innerHTML), 1);
+            setBlogType(blogTypeTemp);
+        }
+    }
+
     return (
         <div>
             <Navbar />
@@ -30,28 +46,33 @@ const BlogSearch = () => {
             <div className='blogSearchContainer'>
                 <div className="navbar-below">
                     <ul className="tags-bg">
-                        <li><div className="tag-bg">Top Blogs</div></li>
-                        <li><div className="tag-bg">Travel Blogs</div></li>
-                        <li><div className="tag-bg">Coding Blogs</div></li>
-                        <li><div className="tag-bg">Tech Blogs</div></li>
-                        <li><div className="tag-bg">Food Blogs</div></li>
-                        <li><div className="tag-bg">Travel Blogs</div></li>
-                        <li><div className="tag-bg">Music Blogs</div></li>
-                        <li><div className="tag-bg">Movie Blogs</div></li>
+                        <li><div className="crimson" onClick={handleClick}>Top Blogs</div></li>
+                        <li><div className="tag-bg" onClick={handleClick}>Travel Blogs</div></li>
+                        <li><div className="tag-bg" onClick={handleClick}>Coding Blogs</div></li>
+                        <li><div className="tag-bg" onClick={handleClick}>Tech Blogs</div></li>
+                        <li><div className="tag-bg" onClick={handleClick}>Food Blogs</div></li>
+                        <li><div className="tag-bg" onClick={handleClick}>Travel Blogs</div></li>
+                        <li><div className="tag-bg" onClick={handleClick}>Music Blogs</div></li>
+                        <li><div className="tag-bg" onClick={handleClick}>Movie Blogs</div></li>
                     </ul>
                     <div class="drop-hide">
-                         <select class="sortByDropdown-select" placeholder="Sort By" name="sortByDropdown">
-                        <option value="" disabled selected hidden>Sort Blogs By</option>
-                        <option value="">Upvotes</option>
-                        <option value="">Length</option>
-                        <option value="">Date</option>
-                        <option value="">None</option>
-                    </select>
+                        <select class="sortByDropdown-select" placeholder="Sort By" name="sortByDropdown">
+                            <option value="" disabled selected hidden>Sort Blogs By</option>
+                            <option value="">Upvotes</option>
+                            <option value="">Length</option>
+                            <option value="">Date</option>
+                            <option value="">None</option>
+                        </select>
                     </div>
                 </div>
+                {blogType.map((item) => {
+                        return (
+                            <h2>{item}</h2>
+                        )
+                })}
+                <hr style={{ width: "100%" }} />
             </div>
-            <hr style={{ width: "100%"}}/>
-
+           
         </div>
     );
 }
